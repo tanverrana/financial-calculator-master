cd6aa5b7585a659317222e69db67508ef4d873fd
tanverrana <tanversobur@gmail.com>
2022-02-17 03:43:18 +0600

    error message

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Function For Invput Value
 function getInputValue(value) {
 
     const inputValue = document.getElementById(value);
@@ -5,6 +6,7 @@ function getInputValue(value) {
     const nameValueAmmount = parseFloat(inputValueText);
     return nameValueAmmount;
 }
+//Function For calculate Value
 function calculateTotal() {
     const income = getInputValue("income-input");
     const food = getInputValue("food-input");
@@ -12,23 +14,28 @@ function calculateTotal() {
     const cloths = getInputValue("cloths-input");
     const failError = document.getElementById('notify-fail');
     const incomeError = document.getElementById('balance-fail');
+    //Error Handleing Part
     if (income < 0) {
         //alert("please enter a positive number");
         failError.style.display = "block";
         alert("Please enter a positive number in your Food field.")
     }
     else if (food < 0) {
+        failError.style.display = "block";
         alert("Please enter a positive number in your Food field.")
     }
     else if (rent < 0) {
+        failError.style.display = "block";
         alert("Please enter a positive number in your Rent field.")
     }
     else if (cloths < 0) {
+        failError.style.display = "block";
         alert("Please enter a positive number in your Cloths field.")
     }
     else {
         const totalExpense = food + rent + cloths;
         const balance = income - totalExpense;
+        //Error Handle through income and Expenses
         if (totalExpense > income) {
             incomeError.style.display = "block";
 
@@ -41,29 +48,41 @@ function calculateTotal() {
 
 
 }
+//Calculate Save Total
 function calculateSaveTotal() {
     const income = getInputValue("income-input");
     const food = getInputValue("food-input");
     const rent = getInputValue("rent-input");
     const cloths = getInputValue("cloths-input");
     const save = getInputValue("save-input");
-    const totalExpense = food + rent + cloths;
-    const balance = income - totalExpense;
-    const saveBalance = income * (save / 100);
-    const remainingBalance = balance - saveBalance;
-    if (saveBalance > balance) {
-        const failError = document.getElementById('save-fail');
-        failError.style.display = "block";
+    //Error Handle negative value & compare Main balance and saving balance.
+    if (save < 0) {
+        const saveError = document.getElementById('save-error');
+        saveError.style.display = "block";
     }
     else {
-        document.getElementById("saving-balance").innerText = saveBalance;
-        document.getElementById("remaining-balance").innerText = remainingBalance;
+        const totalExpense = food + rent + cloths;
+        const balance = income - totalExpense;
+        const saveBalance = income * (save / 100);
+        const remainingBalance = balance - saveBalance;
+        if (saveBalance > balance) {
+            const failError = document.getElementById('save-fail');
+            failError.style.display = "block";
+        }
+        else {
+            document.getElementById("saving-balance").innerText = saveBalance;
+            document.getElementById("remaining-balance").innerText = remainingBalance;
+        }
     }
+
+
 }
+//Even listner add calculate button
 
 document.getElementById("calculate-button").addEventListener("click", function () {
     calculateTotal();
 });
+//Even listner add Save button
 document.getElementById("save-balance").addEventListener("click", function () {
     calculateSaveTotal();
 })
